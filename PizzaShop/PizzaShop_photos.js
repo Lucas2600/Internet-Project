84f925ce738f380d28909de213eafdb68ed23e15
Lucas2600 <38708498+Lucas2600@users.noreply.github.com>
2018-04-24 20:51:17 -0400

    Add files via upload
    first version with pizza

---
+++ PizzaShop/PizzaShop_photos.js
@@ -0,0 +1,28 @@
+//-------------------------- START --------------------------//
+
+$(document).ready(function()
+{
+  $('.next').on('click', function()
+  {
+    var currentImg = $('img.active');
+    var nextImg = currentImg.next();
+
+    if(nextImg.length)
+	{
+      currentImg.removeClass('active').css('z-index', -10);
+      nextImg.addClass('active').css('z-index', 10);
+    }
+  });
+
+  $('.prev').on('click', function()
+  {
+    var currentImg = $('img.active');
+    var prevImg = currentImg.prev();
+
+    if(prevImg.length)
+	{
+      currentImg.removeClass('active').css('z-index', -10);
+      prevImg.addClass('active').css('z-index', 10);
+    }
+  });
+});
